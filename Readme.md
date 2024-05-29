938f2b6ba80d3d2915ebb69f748c8cb4bb6b739e
GuangyaoZhang <xjtu521@qq.com>
2024-05-29 10:06:14 +0000

    Add Bandwidth Test and Readme.md

--- Readme.md
+++ Readme.md
@@ -1,3 +1,15 @@
-TBD
+## 任务描述
 
-First Pull Request
+写一个脚本测试单机多卡(2，4，8卡)以及多机多卡（16卡）的GPU带宽，能够测量all-reduce, all-gather，broadcast和all-to-all的不同通信原语下的通信带宽，只能使用PyTorch以及python内置的包。
+
+
+## 任务结果
+
+
+| 指标      |    2卡 | 4卡  |8卡  | 16卡|
+| :--- | ---:| :--: |:--: |:--: |
+|all-to-all   | 617 |436|369|
+| all-reduce  | 319| 224|253|
+all-gather     |369| 313|290|
+broadcast     | 344| 338|338|
+
---
+++ dist_bandwidth.py
@@ -0,0 +1,65 @@
+import torch.distributed as dist
+import torch
+
+def setup():
+
+    dist.init_process_group(backend="nccl")
+    rank = dist.get_rank()
+    torch.cuda.set_device(rank)
+
+
+def prepare_all_to_all(size = 16384):
+    op = dist.all_to_all
+    world_size = dist.get_world_size()
+    input_tensor = list(torch.rand(world_size, size, size).cuda().chunk(world_size))
+    output_tensor = list(torch.rand(world_size, size, size).cuda().chunk(world_size))
+    return 'all_to_all',[output_tensor, input_tensor], op, world_size*size*size
+
+    
+def prepare_all_reduce(size = 16384):
+    op = dist.all_reduce
+    world_size = dist.get_world_size()
+    input_tensor = torch.rand(world_size, size, size).cuda()
+    return 'all_reduce', [input_tensor], op, world_size*size*size
+
+def prepare_all_gather(size = 16384):
+    op = dist.all_gather
+    world_size = dist.get_world_size()
+    input_tensor = torch.rand(size, size).cuda()
+    output_tensor = list(torch.rand(world_size, size, size).cuda().chunk(world_size))
+    
+    return 'all_gather', [output_tensor, input_tensor], op, world_size*size*size
+
+def prepare_broadcast(size = 16384):
+    op = dist.broadcast
+    world_size = dist.get_world_size()
+    input_tensor = torch.rand(world_size, size, size).cuda()
+    src = 0
+
+    return 'broadcast', [input_tensor, src], op, world_size*size*size
+
+
+
+import time
+if __name__ == "__main__":
+    setup()
+    for op_func in [prepare_all_to_all, prepare_all_reduce, prepare_all_gather, prepare_broadcast]:
+        op_name, args, op, size = op_func()
+
+        op(*args)
+        
+        dist.barrier()
+        time1 = time.time()
+        for i in range(10):
+            op(*args)
+            dist.barrier()
+        dist.barrier()
+        time2 = time.time()
+        duration = (time2-time1)/10
+        bandwidth = 4*size/duration/(1024*1024*1024)
+        if dist.get_rank()==0:
+
+            print(op_name, bandwidth,"GB/s")
+
+
+    
